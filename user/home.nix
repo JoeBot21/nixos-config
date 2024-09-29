ba9c2313e2c15cc8a2f24fa06dd874e9aef7b858
JoeBot21 <joe.bears77@gmail.com>
2024-09-29 18:33:08 -0400

    Add conda to path

--- user/home.nix
+++ user/home.nix
@@ -50,6 +50,10 @@
     # '';
   };
 
+  home.sessionPath = [
+    "/home/joebot/.conda/condabin"
+  ];
+
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
